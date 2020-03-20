35c4757cc6cd94533fceea4e47f65901d73225bc
Jan Bednařík <jan.bednarik@gmail.com>
2020-03-21 00:05:04 +0100

    Command load_groups

---
+++ main/config.py
@@ -0,0 +1,23 @@
+REGION_ADMIN = {
+    "group_name": "Správce oblasti",
+    "permissions": [
+        "change_region",
+        "view_region",
+        "view_regionadmin",
+        "add_location",
+        "change_location",
+        "view_location",
+        "add_locationstaff",
+        "change_locationstaff",
+        "delete_locationstaff",
+        "view_locationstaff",
+        "add_material",
+        "change_material",
+        "view_material",
+        "add_materialrecord",
+        "change_materialrecord",
+        "view_materialrecord",
+    ],
+}
+
+GROUPS = [REGION_ADMIN]
---
+++ main/management/commands/load_groups.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import Group, Permission
+from django.core.management.base import BaseCommand
+
+from ...config import GROUPS
+
+
+class Command(BaseCommand):
+    help = "Nahraje do databáze skupiny pro oprávnění"
+
+    def handle(self, *args, **options):
+        self.stdout.write("Nahrávám skupiny pro oprávnění...")
+
+        for config in GROUPS:
+            self.stdout.write(f" - {config['group_name']}")
+            group, _ = Group.objects.get_or_create(name=config["group_name"])
+            permissions = Permission.objects.filter(codename__in=config["permissions"])
+            group.permissions.add(*list(permissions))
