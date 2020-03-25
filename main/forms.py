0d462e702085bede0c52846191f94540824ec609
Jan Bednařík <jan.bednarik@gmail.com>
2020-03-25 01:17:27 +0100

    Update dispense start form

--- main/forms.py
+++ main/forms.py
@@ -6,31 +6,30 @@ from .models import Dispensed, Location, Material
 
 
 class LoginForm(forms.Form):
-    email = forms.CharField(label="email", max_length=100)
+    email = forms.CharField(label="Email", max_length=100)
     password = forms.CharField(
-        label="heslo", widget=forms.PasswordInput(), max_length=100
+        label="Heslo", widget=forms.PasswordInput(), max_length=100
     )
-    location = forms.ModelChoiceField(label="lokalita", queryset=Location.objects.all())
+    location = forms.ModelChoiceField(label="Lokalita", queryset=Location.objects.all())
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_class = "form-horizontal"
-        self.helper.label_class = "col-lg-2"
-        self.helper.field_class = "col-lg-7"
-        self.helper.add_input(Submit("submit", "přihlásit"))
+        self.helper.label_class = "col-lg-1"
+        self.helper.field_class = "col-lg-4"
+        self.helper.add_input(Submit("submit", "Přihlásit"))
 
 
 class DispenseStartForm(forms.Form):
-    id_card_no = forms.IntegerField(label="číslo dokladu")
+    id_card_no = forms.IntegerField(label="Číslo dokladu")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.form_class = "form-horizontal"
-        self.helper.label_class = "col-lg-2"
-        self.helper.field_class = "col-lg-3"
-        self.helper.add_input(Submit("submit", "ověřit"))
+        self.helper.form_class = "form-inline mb-4"
+        self.helper.field_class = "ml-3"
+        self.helper.add_input(Submit("submit", "Ověřit"))
 
 
 class DispenseNewForm(forms.Form):
